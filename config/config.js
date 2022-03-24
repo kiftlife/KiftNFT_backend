454c69d6e56fa33df097028b13c3c6cc9611dfe5
Sante Kotturi <sakotturi@gmail.com>
2022-03-24 14:28:39 -0500

    added teams wallets

--- config/config.js
+++ config/config.js
@@ -13,14 +13,16 @@ const airdropDevAddresses = [
     '0x15d34aaf54267db7d7c367839aaf71a00a2c6a65'
   ];
 
-  const airdropTestAddresses = [
+  const testAddresses = [
     '0x93E40A70115C9EfC67f817BfC62717d8Ab66C720',     // brad
-    '0xF912DF89aFD526B451bF50fA7c0477bCe1A7b10A'      //sante
+    '0xF912DF89aFD526B451bF50fA7c0477bCe1A7b10A',     // sante
+    '0x9de5ae3b772bcf4661f8b307678a90668729c89e',     // lev
+    '0x6F20F74586936B09cc6E21e35067ad5Aa197b130',     // nick
+    '0x025B62EB00120e340B30F8c7Ba0f3b20392edA90',     // mo
+    '0xc0c385e74C47e19302Be184Af3379fc21FBf079B',     // cam
+    '0xc2B27c40D37d1d8338525656D5570A397Bc155E5',     // colin
   ];
-  const communityTestAddresses = [
-    '0x93E40A70115C9EfC67f817BfC62717d8Ab66C720',     // brad
-    '0xF912DF89aFD526B451bF50fA7c0477bCe1A7b10A'      //sante
-  ];
+
 
   const airdropProdAddresses = [];
   const communityProdAddresses = [];
@@ -30,8 +32,7 @@ const airdropDevAddresses = [
     IPFS_BASE_URL,
     airdropDevAddresses,
     communityDevAddresses,
-    airdropTestAddresses,
-    communityTestAddresses,
+    testAddresses,
     airdropProdAddresses,
     communityProdAddresses
   }
