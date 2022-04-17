b3c8528bce875a8031db5ae9e0e7d622f057e97e
Sante Kotturi <sakotturi@gmail.com>
2022-04-17 12:19:48 -0700

    clean up

--- test/batchReveal.js
+++ test/batchReveal.js
@@ -4,40 +4,32 @@ const { BASE_PREREVEAL_URL, CHAINLINK_KEY_HASH } = require('../config/config');
 const { constants } = ethers;
 
 function generateTokenIdArray(start) {
-    return Array.from({ length: 10 }, (_, i) => i + start);
-  }
+  return Array.from({ length: 10 }, (_, i) => i + start);
+}
 
 async function asyncForEach(array, callback) {
-    const res = [];
-    for (let index = 0; index < array.length; index++) {
-      res.push(await callback(array[index], index, array));
-    }
-    return res;
+  const res = [];
+  for (let index = 0; index < array.length; index++) {
+    res.push(await callback(array[index], index, array));
   }
-  
+  return res;
+}
 
 describe('BatchReveal', async () => {
   it('Successfully batch reveal 10000 tokens', async () => {
+    // setup
     const [owner, addr1, addr2, addr3, addr4, addr5] =
       await ethers.getSigners();
     const MOCK_SUBSCRIPTION_ID = 0;
-    const MOCK_LINK = constants.AddressZero;
-
-    console.log('Mock link: ', MOCK_LINK);
-
+    // const MOCK_LINK = constants.AddressZero;        // not needed
     const vrfCoordinatorContract = 'MockVRFCoordinator';
 
+    // deploy
     const kiftContractFactory = await ethers.getContractFactory('Kiftables');
-
     const vrfCoordFactory = await ethers.getContractFactory(
       vrfCoordinatorContract
     );
     const mockVrfCoordinator = await vrfCoordFactory.connect(owner).deploy();
-
-    console.log(
-      'Mock VRF Coordinator Deployed at: ',
-      mockVrfCoordinator.address
-    );
 
     const kiftContract = await kiftContractFactory
       .connect(owner)
@@ -48,8 +40,14 @@ describe('BatchReveal', async () => {
         MOCK_SUBSCRIPTION_ID
       );
 
+    console.log(
+      'Mock VRF Coordinator Deployed at: ',
+      mockVrfCoordinator.address
+    );
     console.log('KiftContract: ', kiftContract.address);
+    // end deploy
 
+    // check airdrop
     const maxAirdroppedVans = 100;
     const numToAirdrop = 10;
 
@@ -71,28 +69,14 @@ describe('BatchReveal', async () => {
     expect(ownerBalance).to.equal(
       maxAirdroppedVans - addresses.length * numToAirdrop
     );
+
+    // TODO confirm metadata uri is still not revealed
+    // end airdrop
+
+    
+    // TODO mint next 900 tokens triggering a batch
+    // TODO confirm metadata uri is revealed for token 1-1000;
+    // TODO mint all the way up to 10000 and confirm not overflows/offset issues
+    
   });
-
-  //   async function deployContract(vrfCoordinatorContract = 'MockVRFCoordinator') {
-  //     const kiftContractFactory = await ethers.getContractFactory('kiftContract');
-
-  //     const vrfCoordFactory = await ethers.getContractFactory(
-  //       vrfCoordinatorContract
-  //     );
-  //     const mockVrfCoordinator = await vrfCoordFactory.connect(owner).deploy();
-
-  //     console.log(
-  //       'Mock VRF Coordinator Deployed at: ',
-  //       mockVrfCoordinator.address
-  //     );
-
-  //     return await kiftContractFactory
-  //       .connect(owner)
-  //       .deploy(
-  //         BASE_PREREVEAL_URL,
-  //         mockVrfCoordinator.address,
-  //         MOCK_LINK,
-  //         MOCK_SUBSCRIPTION_ID
-  //       );
-  //   }
 });
