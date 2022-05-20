c83e0c910bd177abecac3deb2109c342480cf008
Bradley Ryan <bradryan7@gmail.com>
2022-05-20 15:02:07 -0700

    BOOM - graph api

---
+++ kiftables_subgraph/src/mapping.ts
@@ -0,0 +1,33 @@
+import { ipfs, json } from '@graphprotocol/graph-ts'
+import {
+  Transfer as TransferEvent,
+  Kiftables as KiftablesContract
+} from '../generated/Kiftables/Kiftables'
+import { Token, User } from '../generated/schema'
+
+
+
+export function handleTransfer(event: TransferEvent): void {
+  console.info(`Received Transfer event ${event.address}`)
+
+  /* load the token from the existing Graph Node */
+  let token = Token.load(event.params.tokenId.toString())
+  if (!token) {
+    /* if the token does not yet exist, create it */
+    token = new Token(event.params.tokenId.toString())
+    token.tokenID = event.params.tokenId
+
+    token.updatedAtTimestamp = event.block.timestamp
+
+    /* set or update the owner field and save the token to the Graph Node */
+    token.owner = event.params.to.toHexString()
+    token.save()
+  }
+  
+  /* if the user does not yet exist, create them */
+  let user = User.load(event.params.to.toHexString())
+  if (!user) {
+    user = new User(event.params.to.toHexString())
+    user.save()
+  }
+}
